5574732f4d95a835454f14f62b2f7b985b164fe4
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-04-28 14:52:58 +0700

    Update configuration of LB

--- etc/bin/deploy.sh
+++ etc/bin/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+INGRESS_IP=$(kubectl get -n jsc-ns ing -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}')
+
+sed "s/IP_ADDRESS/${INGRESS_IP}/g" ./etc/yaml/jsc-k3s-dashboard/04-service-example.yaml > ./etc/yaml/jsc-k3s-dashboard/04-service.yaml
+
 # Deploy the application
 kubectl apply -k ./etc/yaml/jsc-k3s-dashboard/
