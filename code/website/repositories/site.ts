98a64cdf152bed9aaf234c64edbdba4e187bd8d1
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-08-07 20:47:55 +0700

    Try fix site name environment variable.

--- code/website/repositories/site.ts
+++ code/website/repositories/site.ts
@@ -1,3 +1,3 @@
-export const siteName = process.env.SITE_NAME || 'localhost';
+export const siteName = process.env.NEXT_PUBLIC_SITE_NAME || 'localhost';
 
 export const siteUrl = `https://${siteName}`;
