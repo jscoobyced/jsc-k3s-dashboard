ff90fc8a23ba4e00a62860c78b08f4aea298754e
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-09-14 14:30:29 +0700

    Cleanup setup

--- etc/bin/setup.sh
+++ etc/bin/setup.sh
@@ -8,11 +8,16 @@ SERVERSIDE="serverside"
 
 build_web() {
     echo "  🛠️   Building $1"
+    # Reset package.json
     cp src/$1/package.tpl.json src/$1/package.json
+    # Delete older node_modules and yarn.lock
     rm -Rf ./src/$1/node_modules ./src/$1/yarn.lock
+    # Format dependencies to a single line
     DEV_FILES=$(cat src/$1/deps_dev.txt | tr '\n' ' ')
     RUN_FILES=$(cat src/$1/deps_run.txt | tr '\n' ' ')
+    # Install dependencies
     BUILD_SRC=$1 docker-compose run --rm build-web yarn --cwd /app/src/$1 add $RUN_FILES > /dev/null
+    # Install dev dependencies
     BUILD_SRC=$1 docker-compose run --rm build-web yarn --cwd /app/src/$1 add -D $DEV_FILES > /dev/null
 }
 
