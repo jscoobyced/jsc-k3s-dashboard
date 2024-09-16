ecdae605266c948e523fda3ed63ddf65d23bd675
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-09-16 22:13:37 +0700

    Add dynamic route

--- src/clientside/src/services/routing/apiRoutes.ts
+++ src/clientside/src/services/routing/apiRoutes.ts
@@ -1,6 +1,6 @@
 export const apiDomain =
   window.location.hostname === 'localhost'
-    ? '/'
+    ? '//localhost:3000/'
     : `//api.${window.location.hostname}/`;
 export const pageRoutes = `${apiDomain}api/pages`;
 
--- src/serverside/src/index.ts
+++ src/serverside/src/index.ts
@@ -1,9 +1,21 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import { errorRoute } from './routes/error';
 import { homeRoute } from './routes/home';
 
+// Get environent variables
+dotenv.config();
+const domain = process.env.DOMAIN ?? 'localhost';
+const port = process.env.PORT ?? '5173';
+const scheme = process.env.SCHEME ?? 'http';
+
 // Create Express app
 export const app = express();
+var corsOptions = {
+  origin: `${scheme}://${domain}:${port}`,
+};
+app.use(cors(corsOptions));
 
 // Bind routes
 app.use('/', homeRoute);
