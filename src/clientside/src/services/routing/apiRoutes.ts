22ecf980122d44a974a23e9e4eb2529fe3a82d4b
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-09-16 21:44:42 +0700

    Test using the api domain dynamically.

--- src/clientside/src/services/routing/apiRoutes.ts
+++ src/clientside/src/services/routing/apiRoutes.ts
@@ -1,4 +1,8 @@
-export const pageRoutes = '/api/pages';
+export const apiDomain =
+  window.location.hostname === 'localhost'
+    ? '/'
+    : `//api.${window.location.hostname}/`;
+export const pageRoutes = `${apiDomain}api/pages`;
 
 export const commonRoute = 'common';
 export const homeRoute = 'home';
